8fbdcc001cbdd4f6a19236a35cbcb7ec331fab8d
Toleda <none>
2021-02-18 14:11:31 +0100

    v1.1 config in yaml, fixes...

--- anoncsv.py
+++ anoncsv.py
@@ -1,36 +1,46 @@
 #coding:utf-8
-import conf
+'''
+AnonCSV
+Programme d'anonymisation de fichiers de données au format CSV.
+
+
+
+
+
+'''
 import os
 import pandas
 import random
-import shutil
-import sys
 import time
-
-ORIGINAL_EXT        = ".orig.csv"                   # Testing purpose only...
-CURRENT_DIR         = os.path.join(sys.path[0])
+import yaml
 
 
-'''
-FONCTION ANONYMIZE
-Cette fonction prend un nom de fichier csv en argument, et le lis avec Pandas.
-Pour chaque colonne définie dans dans les clés du dictionnaire COLUMN_TO_ANONYMIZE (conf.py), remplace l'ensemble
-des données de la colonne selon un traitement appelé par la valeur correspondant à la clé (LASTNAME, "intXX", etc.).
-Une fois les données anonymisées, le fichier CSV est réécrit/écrasé avec les nouvelles valeurs.
-'''
-def anonymize(file_to_anon: str):
+# Load configuration
+with open('config.yaml', encoding='utf-8') as yfile:
+    try:
+        config = yaml.safe_load(yfile)
+    except yaml.parser.ParserError:
+        print("Error: wrong YAML format. Check config.yaml.")
+        quit()
+
+
+def anonymize(file_url: str):
     '''
-    Anonymize the file in argument
+    FONCTION ANONYMIZE
+    Cette fonction prend un nom de fichier CSV en argument, et le lis avec Pandas.
+    Pour chaque colonne définie dans dans les clés du dictionnaire COLUMN_TO_ANONYMIZE (config['py), remplace l'ensemble
+    des données de la colonne selon un traitement appelé par la valeur correspondant à la clé (LASTNAME, "intXX", etc.).
+    Une fois les données anonymisées, le fichier CSV est réécrit/écrasé avec les nouvelles valeurs.
     '''
-    
-    file_url    = os.path.join(f'{CURRENT_DIR}\\{file_to_anon}')
     csv_file    = pandas.read_csv(file_url,
-                                  sep       =conf.CSV_DELIMITER,
-                                  quotechar =conf.CSV_QUOTE_CHAR,
-                                  escapechar=conf.CSV_ESCAPE_CHAR).copy()
+                                  sep       =config['CSV_DELIMITER'],
+                                  quotechar =config['CSV_QUOTE_CHAR'],
+                                  escapechar=config['CSV_ESCAPE_CHAR'],
+                                  encoding  ='utf-8').copy()
 
-    for key in conf.COLUMN_TO_ANONYMIZE:
-        vcta = conf.COLUMN_TO_ANONYMIZE[key] # Valeur de remplacement, si spécifiée en constante dans conf.py
+    for key in config['COLUMN_TO_ANONYMIZE']:
+        vcta = config['COLUMN_TO_ANONYMIZE'][key] # Valeur de remplacement, si spécifiée en constante dans config['py
+
         try:
             data = csv_file[key].copy()
 
@@ -69,9 +79,10 @@ def anonymize(file_to_anon: str):
             # Write changes to csv file
             csv_file.to_csv(file_url,
                             index     =False,
-                            sep       =conf.CSV_DELIMITER,
-                            quotechar =conf.CSV_QUOTE_CHAR,
-                            escapechar=conf.CSV_ESCAPE_CHAR)
+                            sep       =config['CSV_DELIMITER'],
+                            quotechar =config['CSV_QUOTE_CHAR'],
+                            escapechar=config['CSV_ESCAPE_CHAR'],
+                            encoding  ='utf-8')
 
             print(f"\tAnonymize {data.name}")
         except KeyError:
@@ -79,44 +90,40 @@ def anonymize(file_to_anon: str):
 
 
 
-
-'''''''''''''''''''''''''''''''''''''''''''''
-
-Testing purpose only... (restaure les fichiers originaux à chaque lancement du script)
-
-'''''''''''''''''''''''''''''''''''''''''''''
-for file in os.listdir(CURRENT_DIR):
+''' Testing purpose only... (restaure les fichiers originaux à chaque lancement du script) '''
+import shutil
+ORIGINAL_EXT=".orig.csv" # Testing purpose only
+for file in os.listdir(os.getcwd()):
     if file[-4:] != ".csv":
         continue
     elif file[-9:] == ORIGINAL_EXT:
         src= file
-        dst= f'{CURRENT_DIR}\\{file[:-9]}.csv'
+        dst= f'{os.getcwd()}\\{file[:-9]}.csv'
     else:
-        os.remove(f'{CURRENT_DIR}\\{file}')
-        src= f'{CURRENT_DIR}\\{file[0:-4]}.orig.csv'
-        dst= f'{CURRENT_DIR}\\{file}'
-
+        os.remove(f'{os.getcwd()}\\{file}')
+        src= f'{os.getcwd()}\\{file[0:-4]}.orig.csv'
+        dst= f'{os.getcwd()}\\{file}'
     shutil.copyfile(src, dst)
 
 
 '''
-MAIN
--> PROGRAM STARTS HERE <-
+MAIN / Program starts here
 '''
+csv_dir   = os.getcwd() + config['CSV_DIR']
 file_list = []
 
-for filename in os.listdir(CURRENT_DIR):
-    if filename[-9:] == ORIGINAL_EXT or filename[-4:] != ".csv":
-        continue
-
-    file_list.append(filename)
-
+for filename in os.listdir(csv_dir):
+    if filename[-9:]==ORIGINAL_EXT:continue # Testing purpose only
+    if filename[-4:] == ".csv":
+        file_list.append(filename)
 
 for filename in file_list:
+    file_url    = os.path.join(f'{csv_dir}{filename}')
+
     print(f"{filename} -------------")
 
     timer_start = time.time()
-    anonymize(filename)
+    anonymize(file_url)
     timer_end = time.time() - timer_start
 
     print(f"\t{format(timer_end, '.3f')}s")
--- readme.md
+++ readme.md
@@ -1,50 +1,64 @@
 # AnonCSV
 Ce programme écrit en python permet d'anonymiser les données d'un fichier de données CSV.
 Le programme est composé de deux fichiers :
-- anoncsv.py : programme principal0
-- conf.py : fichier de configuration/paramétrage 
+- anoncsv.py : programme
+- config.yaml : fichier de configuration/paramétrage 
 
 ## Installation, exécution
-- Copier anoncsv.py et conf.py dans un dossier,
-- Placer les CSV à anonymiser à la racine du même dossier,
-- Paramétrer conf.py (voir plus bas)
+- Copier anoncsv.py et config.yaml dans un dossier.
+- Paramétrer config.yaml (voir plus bas).
+- Par défaut, placer les CSV à anonymiser dans un dossier "csv" créé à côté du programme. Ce répertoire peut être modifié dans la configuré.
 - Exécuter anoncsv.py
 
-## Configuration (conf.py)
-Les paramètres du programmes sont stockés sous forme de constantes dans le fichier **conf.py**.
-Les premières constantes permettent de définir le format du csv (séparateur, caractère de quote, caractère d'échappement) et les listes de données (nom, prénom, commentaires) dans lesquels le programme choisira aléatoirement les données de substitution (anonymes).
+## Configuration (config.yaml)
+Les paramètres du programmes sont stockés dans le fichier **config.yaml** : dossier contenant les fichiers CSV, séparateur CSV, caractère de quote, caractère d'échappement.
 
-Par exemple, des noms de famille figurant sur un fichier CSV à anonymiser peuvent être remplacés par un nom issu de la liste "LASTNAME", choisi aléatoirement par AnonCSV.
+Des **listes** de données de substitution (anonymes) peuvent être définies. Dans la partie suivantes, on pourra indiquer au programme de choisir aléatoirement des valeurs dans ces listes, en remplacement des données d'origine.
 
-```python
-LASTNAME  = ("Martin","Bernard","Thomas", ...)
+Par exemple, des noms de famille figurant sur un fichier CSV à anonymiser pourront être remplacés par un nom issu de la liste `LASTNAME`, choisi aléatoirement par AnonCSV.
+
+```yaml
+LASTNAME : &LASTNAME [Martin,Bernard,Thomas, ...]
 ```
 
+Trois listes sont fournies par défaut : une liste de noms de famille, de prénoms, et de commentaires.
+
 #### Colonnes à anonymiser
-La constante "COLUMN_TO_ANONYMIZE" est une liste de type dictionnaire (une liste de "clé:valeur") à paramétrer selon le contenu des CSV à anonymiser.
+`COLUMN_TO_ANONYMIZE` est une liste de type dictionnaire (une liste de clés associés à une valeur). Cette liste doit être paramétrée selon le contenu des CSV à anonymiser.
 
-#### Exemple
+---
 
+## Exemple
 Fichier CSV avant anonymisation :
+
+|id|NOMPERS|PRENOMPERS|observation|salaire|
+|--|-------|----------|-----------|-------|
+|0000081|chirac|patrick|"D'un naturel ""jovial"", Patrick Chirac est un excellent élément."|1450|
+|0000082|norris|chuck|"A su compter jusqu'à l'infini deux fois."|9050|
 ```csv
-id,nompers,prenompers,observation
+id,NOMPERS,PRENOMPERS,observation,salaire
 0000081,chirac,patrick,"D'un naturel ""jovial"", Patrick Chirac est un excellent élément.",1450
 0000082,norris,chuck,"A su compter jusqu'à l'infini deux fois.",9050
 ```
 
-conf.py :
-```python
-                        #   Clé: nom de colonne     Valeur remplacement
-COLUMN_TO_ANONYMIZE =   {   "NOMPERS":              LASTNAME,
-                            "PRENOMPERS":           FIRSTNAME,
-                            "OBSERVATION":          COMMENT,
-                            "SALAIRE":              "int4"
-                        }
+config.yaml :
+```yaml
+COLUMN_TO_ANONYMIZE:
+#   Clé: nom de colonne   Valeur de remplacement
+    NOMPERS:              *LASTNAME
+    PRENOMPERS:           *FIRSTNAME
+    observation:          *COMMENT
+    salaire:              int4
 ```
+<small>Par exemple, on indique ici que pour tous les fichiers CSV trouvés, si l'on trouve une (ou plusieurs) colonne nommée `NOMPERS`, le programme remplacera ses valeurs par l'une des valeurs de la liste `LASTNAME` définie plus haut.</small>
 
 Fichier CSV après exécution AnonCSV :
+|id|NOMPERS|PRENOMPERS|observation|salaire|
+|--|-------|----------|-----------|-------|
+|81|Bernard|Léo|"Un terminator."|4842|
+|82|Thomas|Gabriel|"Qui est cette personne ?"|1254|
 ```csv
-id,nompers,prenompers,observation
+id,NOMPERS,PRENOMPERS,observation,salaire
 81,Bernard,Léo,"Un terminator.",4842
 82,Thomas,Gabriel,"Qui est cette personne ?",1254
 ```
